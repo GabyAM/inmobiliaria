09b788236e8ace76415bd6f91f561d5414e60e8d
GabyAM <gdgabyam@gmail.com>
2024-05-29 17:42:36 -0300

    añade logo en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import '../assets/styles/header.css';
 export function Header() {
     return (
         <header>
+            <img
+                className="logo"
+                src="/logo-inmobiliaria.svg"
+                alt="Logo de la inmobiliaria"
+            ></img>
         </header>
     );
 }
