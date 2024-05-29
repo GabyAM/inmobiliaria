869cf49043eb0b7f48ea4fe3708101bcf45cdfde
GabyAM <gdgabyam@gmail.com>
2024-05-29 17:40:29 -0300

    Estila la nav bar
    añade iconos e implementa la funcionalidad de expandirla con un botón

---
+++ src/components/Icons.jsx
@@ -0,0 +1,113 @@
+import '../assets/styles/icons.css';
+
+export function HomeIcon({ width = '1em', height = '1em' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+        >
+            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+        </svg>
+    );
+}
+
+export function BuildingIcon({ width = '1em', height = '1em' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="M3 2.25a.75.75 0 0 0 0 1.5v16.5h-.75a.75.75 0 0 0 0 1.5H15v-18a.75.75 0 0 0 0-1.5H3ZM6.75 19.5v-2.25a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75ZM6 6.75A.75.75 0 0 1 6.75 6h.75a.75.75 0 0 1 0 1.5h-.75A.75.75 0 0 1 6 6.75ZM6.75 9a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM6 12.75a.75.75 0 0 1 .75-.75h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 6a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75Zm-.75 3.75A.75.75 0 0 1 10.5 9h.75a.75.75 0 0 1 0 1.5h-.75a.75.75 0 0 1-.75-.75ZM10.5 12a.75.75 0 0 0 0 1.5h.75a.75.75 0 0 0 0-1.5h-.75ZM16.5 6.75v15h5.25a.75.75 0 0 0 0-1.5H21v-12a.75.75 0 0 0 0-1.5h-4.5Zm1.5 4.5a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Zm.75 2.25a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75h-.008ZM18 17.25a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export function ClockIcon({ width = '1em', height = '1em' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export function UsersIcon({ width = '1em', height = '1em' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z"
+                clipRule="evenodd"
+            />
+            <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+        </svg>
+    );
+}
+
+export function PinIcon({ width = '1em', height = '1em' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export function ArrowIcon({ width = '1em', height = '1em', orient = 'right' }) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className={`size-6 arrow-icon ${orient === 'left' ? 'rotated' : ''}`}
+        >
+            <path
+                fillRule="evenodd"
+                d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,52 @@
 import '../assets/styles/navbar.css';
+import { useState } from 'react';
+import {
+    ArrowIcon,
+    BuildingIcon,
+    ClockIcon,
+    HomeIcon,
+    PinIcon,
+    UsersIcon
+} from './Icons';
 import { Link } from 'react-router-dom';
 
 const sections = [
     {
         id: 'tipos_propiedad',
         title: 'Tipos de propiedad',
+        icon: (props) => <HomeIcon {...props}></HomeIcon>,
         url: '/tipos_propiedad'
     },
     {
         id: 'propiedades',
         title: 'Propiedades',
+        icon: (props) => <BuildingIcon {...props}></BuildingIcon>,
         url: '/propiedades'
     },
     {
         id: 'reservas',
         title: 'Reservas',
+        icon: (props) => <ClockIcon {...props}></ClockIcon>,
         url: '/reservas'
     },
     {
         id: 'inquilinos',
         title: 'Inquilinos',
+        icon: (props) => <UsersIcon {...props}></UsersIcon>,
         url: '/inquilinos'
     },
     {
         id: 'localidades',
         title: 'Localidades',
+        icon: (props) => <PinIcon {...props}></PinIcon>,
         url: '/localidades'
     }
 ];
 export function NavBar() {
+    const [isExpanded, setIsExpanded] = useState(true);
     return (
         <div className="main-nav-bar-container">
-            <div className="main-nav-bar">
+            <div className={`main-nav-bar ${isExpanded ? 'expanded' : ''}`}>
                 <div className="nav-bar-links">
                     <ul>
                         {sections.map((section) => (
@@ -40,6 +55,10 @@ export function NavBar() {
                                     className="main-nav-bar-link"
                                     to={section.url}
                                 >
+                                    {section.icon({
+                                        width: '2em',
+                                        height: '2em'
+                                    })}
                                     {isExpanded && <span>{section.title}</span>}
                                 </Link>
                             </li>
@@ -47,6 +66,16 @@ export function NavBar() {
                     </ul>
                 </div>
             </div>
+            <button
+                className="expand-button"
+                onClick={() => setIsExpanded(!isExpanded)}
+            >
+                <ArrowIcon
+                    width="2em"
+                    height="2em"
+                    orient={isExpanded ? 'left' : 'right'}
+                ></ArrowIcon>
+            </button>
         </div>
     );
 }
