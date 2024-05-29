f2647fb9e04d5126c6ecc84736dd18fc8347187e
GabyAM <gdgabyam@gmail.com>
2024-05-29 17:35:28 -0300

    Añade links a la nav bar
    para rediridigir a las diferentes paginas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,51 @@
 import '../assets/styles/navbar.css';
+import { Link } from 'react-router-dom';
+
+const sections = [
+    {
+        id: 'tipos_propiedad',
+        title: 'Tipos de propiedad',
+        url: '/tipos_propiedad'
+    },
+    {
+        id: 'propiedades',
+        title: 'Propiedades',
+        url: '/propiedades'
+    },
+    {
+        id: 'reservas',
+        title: 'Reservas',
+        url: '/reservas'
+    },
+    {
+        id: 'inquilinos',
+        title: 'Inquilinos',
+        url: '/inquilinos'
+    },
+    {
+        id: 'localidades',
+        title: 'Localidades',
+        url: '/localidades'
+    }
+];
 export function NavBar() {
     return (
         <div className="main-nav-bar-container">
             <div className="main-nav-bar">
+                <div className="nav-bar-links">
+                    <ul>
+                        {sections.map((section) => (
+                            <li key={section.id}>
+                                <Link
+                                    className="main-nav-bar-link"
+                                    to={section.url}
+                                >
+                                    {isExpanded && <span>{section.title}</span>}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
