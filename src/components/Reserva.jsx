96887b3d012d08c555cd3c381a792ec43a836abc
Doulos07 <santiiariel2004@gmail.com>
2024-06-11 12:32:38 -0300

    pages_Reservas

---
+++ src/components/Reserva.jsx
@@ -0,0 +1,11 @@
+export function Reserva ({ reserva }){
+    return (
+        <p>hola</p>
+    );
+}
+/*
+a. Propiedad (domicilio, no propiedad_id)
+b. Inquilino(nombre y apellido del inquilino, no inquilino_id)
+c. Fecha desde
+d. Cantidad noches
+e. valor total */
--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,3 +1,42 @@
+import { useFetchData } from '../hooks/useFetchData';
+import { Reserva } from '../components/Reserva'
+
+function fetchReservas (){
+    return fetch ('http://localhost/reservas')
+    .then((res) => {
+        if (!res.ok){
+            throw new Error ('Error al obtener las reservas') 
+        }
+        return res.json();
+    })
+    .then((Response) => Response.data);
+}
+
 export function Reservas() {
-    return <p>Esta es la pagina de reservas</p>;
+    
+    const {
+        data:reservas,
+        isLoading,
+        error
+    } = useFetchData (fetchReservas);
+
+    return (
+        <>
+            <h1>Reservas</h1>
+            <div>{
+                isLoading ? (
+                    <p>carganodo...</p>
+                ) 
+                : error ? (<p>{error.message} </p>) 
+                : (
+                    reservas.map((reserva) =>
+                        <Reserva
+                        key = {reserva.id}
+                        reserva = {reserva}
+                    />
+                    )
+                )}
+            </div>
+        </>
+    );
 }
