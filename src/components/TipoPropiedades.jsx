3724d211ca492d8931ab52c4ed2b0249a973da4f
GabyAM <gdgabyam@gmail.com>
2024-06-12 19:52:07 -0300

    arreglo de conflictos
    cambia todas las referencias de FormSection a contentSection, arregla algunos import.
    CREO que está arreglado

--- src/components/TipoPropiedades.jsx
+++ src/components/TipoPropiedades.jsx
@@ -1,23 +1,24 @@
 import '../assets/styles/tiposPropiedades.css';
 
-export function TipoPropiedades ({ tipoPropiedades }){ //le paso la funcion de pages? , porque va {} TiposPropiedad?
+export function TipoPropiedades({ tipoPropiedades }) {
+    //le paso la funcion de pages? , porque va {} TiposPropiedad?
     return (
-        <div className = "tiposPropiedades">
+        <div className="tiposPropiedades">
             <table>
-               <thead>
+                <thead>
                     <tr>
                         <th>ID</th>
                         <th>TIPOS DE PROPIEDADES</th>
                     </tr>
-               </thead>
-               <tbody>
-               {tipoPropiedades.map((propiedad) => (
-                        <tr key={propiedad.id}> 
+                </thead>
+                <tbody>
+                    {tipoPropiedades.map((propiedad) => (
+                        <tr key={propiedad.id}>
                             <td>{propiedad.id}</td>
                             <td>{propiedad.nombre}</td>
                         </tr>
                     ))}
-               </tbody>
+                </tbody>
             </table>
         </div>
     );
--- src/pages/TiposPropiedad.jsx
+++ src/pages/TiposPropiedad.jsx
@@ -2,35 +2,35 @@ import '../assets/styles/tiposPropiedad.css'; //hacer css
 import { TipoPropiedades } from '../components/TipoPropiedades';
 import { useFetchData } from '../hooks/useFetchData';
 
-function fechTiposPropiedades (){
-    return fetch ('http://localhost/tipo_propiedades')  //Fetch se utiliza para hacer solicitudes HTTP a una API y obtener datos
-    .then((res) => {
-        if (!res.ok) {
-            throw new Error("error al obtener los Tipos de propiedad");
-        }
-        return res.json();
-        }
-    )
-    .then((response)=> response.data); //res y response son lo mismo?
+function fechTiposPropiedades() {
+    return fetch('http://localhost/tipo_propiedades') //Fetch se utiliza para hacer solicitudes HTTP a una API y obtener datos
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('error al obtener los Tipos de propiedad');
+            }
+            return res.json();
+        })
+        .then((response) => response.data); //res y response son lo mismo?
 }
 
 export function TiposPropiedad() {
-    const {data: tiposPropiedad,
+    const {
+        data: tiposPropiedad,
         isLoading, //para indicar si la operacion esta en curso?
         error
-    } =  useFetchData(fechTiposPropiedades);
+    } = useFetchData(fechTiposPropiedades);
 
     return (
         <>
             <h1 className="main-title">Tipos de Propiedades</h1>
             <div>
-                {isLoading ? (  //isLoading cargando boolean
-                    <p>Cargando...</p> 
+                {isLoading ? ( //isLoading cargando boolean
+                    <p>Cargando...</p>
                 ) : error ? (
                     <p>{error.message}</p>
                 ) : (
                     <TipoPropiedades tipoPropiedades={tiposPropiedad} />
-                    )}
+                )}
             </div>
         </>
     );
