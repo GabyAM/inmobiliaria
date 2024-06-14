97cd580f4e06c8027a1fd197cfc342f075046363
GabyAM <gdgabyam@gmail.com>
2024-06-14 09:48:46 -0300

    actualización de propiedades en lista

--- src/components/Propiedad.jsx
+++ src/components/Propiedad.jsx
@@ -1,50 +1,70 @@
+import { Link } from 'react-router-dom';
 import '../assets/styles/propiedad.css';
 import { Etiqueta } from './Etiqueta';
-import { BuildingIcon, PinIcon, UsersIcon } from './Icons';
+import { DeleteIcon, EditIcon } from './Icons';
 
-export function Propiedad({ propiedad }) {
+export function Propiedad({ propiedad, onDeleteClick }) {
     return (
         <div className="propiedad">
             <div className="image-container">
-                <img
-                    src={propiedad.imagen || '/propiedad-image-placeholder.png'}
-                ></img>
+                <Link to={`/propiedades/${propiedad.id}`}>
+                    <img
+                        src={
+                            propiedad.imagen ||
+                            '/propiedad-image-placeholder.png'
+                        }
+                    ></img>
+                </Link>
             </div>
             <div className="propiedad-info">
                 <div className="top-section">
-                    <h2>{propiedad.domicilio}</h2>
+                    <Link to={`/propiedades/${propiedad.id}`}>
+                        <h2>{propiedad.domicilio}</h2>
+                    </Link>
                     <Etiqueta color={propiedad.disponible ? 'celeste' : 'gris'}>
                         {propiedad.disponible ? 'Disponible' : 'No disponible'}
                     </Etiqueta>
                 </div>
                 <div className="mid-section">
                     <div className="sub-section">
-                        <div className="item">
-                            <BuildingIcon
-                                width="1.3em"
-                                height="1.3em"
-                            ></BuildingIcon>
-                            <span>{propiedad.tipo_propiedad.nombre}</span>
-                        </div>
-                        <div className="item">
-                            <UsersIcon width="1.3em" height="1.3em"></UsersIcon>
-                            <span>
-                                {`${propiedad.cantidad_huespedes} ${propiedad.cantidad_huespedes > 1 ? 'Huéspedes' : 'Huésped'}`}
-                            </span>
-                        </div>
+                        <span>{propiedad.tipo_propiedad.nombre}</span>
+                        <span className="section-separator">•</span>
+                        <span>{propiedad.localidad.nombre}</span>
                     </div>
                     <div className="sub-section">
-                        <div className="item">
-                            <PinIcon width="1.3em" height="1.3em"></PinIcon>
-                            <span>{propiedad.localidad.nombre}</span>
-                        </div>
+                        <span>
+                            {`${propiedad.cantidad_huespedes} ${propiedad.cantidad_huespedes > 1 ? 'Huéspedes' : 'Huésped'}`}
+                        </span>
+                        <span className="section-separator">•</span>
+                        <span>${propiedad.valor_noche} por noche</span>
                     </div>
                 </div>
-                {!propiedad.disponible && (
-                    <span className="aviso-disponibilidad">
-                        {`Disponible a partir del ${propiedad.fecha_inicio_disponibilidad}`}
-                    </span>
-                )}
+                <div className="bottom-section">
+                    {!propiedad.disponible && (
+                        <span className="aviso-disponibilidad">
+                            {`Disponible a partir del ${propiedad.fecha_inicio_disponibilidad}`}
+                        </span>
+                    )}
+                    <div className="actions-section">
+                        <Link to={`/propiedades/${propiedad.id}/edit`}>
+                            <button className="edit-button">
+                                <EditIcon
+                                    width="1.3em"
+                                    height="1.3em"
+                                ></EditIcon>
+                            </button>
+                        </Link>
+                        <button
+                            className="delete-button"
+                            onClick={() => onDeleteClick()}
+                        >
+                            <DeleteIcon
+                                width="1.3em"
+                                height="1.3em"
+                            ></DeleteIcon>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
