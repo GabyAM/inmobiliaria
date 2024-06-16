73bfe8e03e83380e3cf8ef40715d0129a78ecac2
GabyAM <gdgabyam@gmail.com>
2024-06-16 17:36:10 -0300

    termina footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import '../assets/styles/footer.css';
+
 export function Footer() {
     return (
         <footer>
-            <p>Este es el footer</p>
+            <h3>Inmobiliaria</h3>
+            <p>Realizado por Gabriel Miranda y Santiago Alvarez</p>
+            <small>
+                <a href="https://github.com/GabyAM/inmobiliaria">
+                    &gt; Link a repositorio &lt;
+                </a>
+            </small>
         </footer>
     );
 }
